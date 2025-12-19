05bd90bf193f204da65143b01c12aee11c74d7da
iawia01 <125351067+iawia01@users.noreply.github.com>
2025-12-19 12:12:01 -0800

    Add files via upload

---
+++ Igbo_dictionary.py
@@ -0,0 +1,34 @@
+Igbo_dictonary ={
+    'money':'ego'
+    'road':'uzo'
+    'joy':'onu'
+    'food':'nri'
+    'vehicle':'ugboala'
+    'water':'mmiri'
+    'friend':'enyi'
+    'market':'ahia'
+    'school':'ulo akwukwo'
+    'mother':'nne'
+    'boy':'nwa'
+    'book':'akwukwo'
+    'shoe':'akpukpo'
+    'house':'ulo'
+    'hospital':'ulo ogwu'
+    'trouser':'uwe ogologo okpa'
+    'good morning':'ututu oma'
+    'good afternoon':'ehihie oma'
+    'mouth':'onu'
+}
+Translation = {
+    'Igbo':'Igbo',
+}
+Language = input('put in the laguage you want to translate')
+words = input('put in the english word you want to translate')
+if Igbo in translation
+Language = translation[Igbo]
+if word in Language
+print('translation:',Language[word])
+else:
+    print('word not found')
+    else:
+        print('language not found')

eef10fc1aa25ad8b17ab5bb9af2e3b8c60378e07
sb5540510-sketch <sb5540510@gmail.com>
2025-12-19 11:35:27 -0800

    Add files via upload

---
+++ Hausa_dictionary.py
@@ -0,0 +1,34 @@
+Hausa={
+    'money':'kudi'
+    'road':'hanya'
+    'joy':'farkin ciki'
+    'food':'abinchi'
+    'vehicle':'mota'
+    'water':'ruwa'
+    'friend':'aboki'
+    'market':'kasuwa'
+    'school':'makaranta'
+    'mother':'uwa'
+    'boy':'yaro'
+    'book':'littafi'
+    'shoe':'talkami'
+    'house':'gida'
+    'hospital':'asibiti'
+    'trouser':'wundo'
+    'good morning':'ina kwana'
+    'good afternoon':'ina wuni'
+    'mouth':'baki'
+}
+Translation = {
+    'Hausa':'Hausa',
+}
+Language = input('put in the laguage you want to translate')
+words = input('put in the english word you want to translate')
+if Hausa in translation
+Language = translation[Hausa]
+if word in Language
+print('translation:',Language[word])
+else:
+    print('word not found')
+    else:
+        print('language not found')

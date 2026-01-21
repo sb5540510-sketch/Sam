8d5aa167b61f0d1583be35f51905e9b2ab9d4811
Joshua Charles Joshua <kaid.cjj@gmail.com>
2026-01-21 01:11:21 +0100

    Update Igbo_dictionary.py

--- Igbo_dictionary.py
+++ Igbo_dictionary.py
@@ -1,34 +1,25 @@
-Igbo_dictonary ={
-    'money':'ego'
-    'road':'uzo'
-    'joy':'onu'
-    'food':'nri'
-    'vehicle':'ugboala'
-    'water':'mmiri'
-    'friend':'enyi'
-    'market':'ahia'
-    'school':'ulo akwukwo'
-    'mother':'nne'
-    'boy':'nwa'
-    'book':'akwukwo'
-    'shoe':'akpukpo'
-    'house':'ulo'
-    'hospital':'ulo ogwu'
-    'trouser':'uwe ogologo okpa'
-    'good morning':'ututu oma'
-    'good afternoon':'ehihie oma'
-    'mouth':'onu'
+IGBO_DICT = {
+    'money': 'ego',
+    'road': 'uzo',
+    'joy': 'onu',
+    'food': 'nri',
+    'vehicle': 'ugboala',
+    'water': 'mmiri',
+    'friend': 'enyi',
+    'market': 'ahia',
+    'school': 'ulo akwukwo',
+    'mother': 'nne',
+    'boy': 'nwa',
+    'book': 'akwukwo',
+    'shoe': 'akpukpo',
+    'house': 'ulo',
+    'hospital': 'ulo ogwu',
+    'trouser': 'uwe ogologo okpa',
+    'good morning': 'ututu oma',
+    'good afternoon': 'ehihie oma',
+    'mouth': 'onu'
 }
-Translation = {
-    'Igbo':'Igbo',
-}
-Language = input('put in the laguage you want to translate')
-words = input('put in the english word you want to translate')
-if Igbo in translation
-Language = translation[Igbo]
-if word in Language
-print('translation:',Language[word])
-else:
-    print('word not found')
-    else:
-        print('language not found')
+
+def translate_to_igbo(word: str):
+    word = word.lower().strip()
+    return IGBO_DICT.get(word, None)

7de19538a6632349fc2216dcc48518e78c68f63a
Joshua Charles Joshua <kaid.cjj@gmail.com>
2026-01-21 01:08:56 +0100

    Update Hausa_dictionary.py

--- Hausa_dictionary.py
+++ Hausa_dictionary.py
@@ -1,34 +1,25 @@
-Hausa={
-    'money':'kudi'
-    'road':'hanya'
-    'joy':'farkin ciki'
-    'food':'abinchi'
-    'vehicle':'mota'
-    'water':'ruwa'
-    'friend':'aboki'
-    'market':'kasuwa'
-    'school':'makaranta'
-    'mother':'uwa'
-    'boy':'yaro'
-    'book':'littafi'
-    'shoe':'talkami'
-    'house':'gida'
-    'hospital':'asibiti'
-    'trouser':'wundo'
-    'good morning':'ina kwana'
-    'good afternoon':'ina wuni'
-    'mouth':'baki'
+HAUSA_DICT = {
+    'money': 'kudi',
+    'road': 'hanya',
+    'joy': 'farinciki',
+    'food': 'abinci',
+    'vehicle': 'mota',
+    'water': 'ruwa',
+    'friend': 'aboki',
+    'market': 'kasuwa',
+    'school': 'makaranta',
+    'mother': 'uwa',
+    'boy': 'yaro',
+    'book': 'littafi',
+    'shoe': 'takalmi',
+    'house': 'gida',
+    'hospital': 'asibiti',
+    'trouser': 'wando',
+    'good morning': 'ina kwana',
+    'good afternoon': 'ina wuni',
+    'mouth': 'baki'
 }
-Translation = {
-    'Hausa':'Hausa',
-}
-Language = input('put in the laguage you want to translate')
-words = input('put in the english word you want to translate')
-if Hausa in translation
-Language = translation[Hausa]
-if word in Language
-print('translation:',Language[word])
-else:
-    print('word not found')
-    else:
-        print('language not found')
+
+def translate_to_hausa(word: str):
+    word = word.lower().strip()
+    return HAUSA_DICT.get(word, None)
